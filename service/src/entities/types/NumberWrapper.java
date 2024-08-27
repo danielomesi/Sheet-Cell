7d14eca4e3ac599d605074c7022c6584d5a395c7
Daniel Omesi <omesidaniel@gmail.com>
2024-08-27 21:11:29 +0300

    Fixed bug in printing negative numbers

--- service/src/entities/types/NumberWrapper.java
+++ service/src/entities/types/NumberWrapper.java
@@ -5,21 +5,21 @@ import java.text.DecimalFormat;
 
 public class NumberWrapper implements Serializable {
     private Number value;
-    public NumberWrapper(double value) {
+    public NumberWrapper(Double value) {
         this.value = value;
     }
-    public double getDoubleValue() {return value.doubleValue();}
+    public Double getDoubleValue() {return value.doubleValue();}
     public void setValue(double value) {this.value = value;}
     public int getIntValue() {return value.intValue();}
 
     @Override
     public String toString() {
-        DecimalFormat wholeNumberFormatter = new DecimalFormat("#,###");
-        DecimalFormat decimalFormatter = new DecimalFormat("#,###.00");
-        if ((value.doubleValue()) % 1 == 0) {
-            return wholeNumberFormatter.format(value);
+        if (value.doubleValue() % 1 == 0) {
+            // For whole numbers with thousands separators
+            return String.format("%,d", value.longValue());
         } else {
-            return decimalFormatter.format(value);
+            // For decimal numbers with thousands separators and 2 decimal places
+            return String.format("%,.2f", value.doubleValue());
         }
     }
 }
