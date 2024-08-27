ce2b74d879c0fe9cc908b1511f62c6063bd3e82f
Daniel Omesi <omesidaniel@gmail.com>
2024-08-27 15:24:30 +0300

    Added name

--- ui/src/console/Application.java
+++ ui/src/console/Application.java
@@ -2,15 +2,7 @@ package console;
 
 import console.menu.Menu;
 
-//to do:
-//think wether plus between nan and a number should be nan? or terminate the calculation process
-//test the references cases - im not sure if it works well
-//try to divide the code to more functions, make it more readable
-//think about encapsulation - working with types
-//C:\\Users\\omesi\\Desktop\\examples\\insurance
-//save/load:
-//C:\\Users\\omesi\\Desktop\\examples\\data
-
+//Daniel Omesi, 207689092
 public class Application {
 
     public static void main(String[] args) {
