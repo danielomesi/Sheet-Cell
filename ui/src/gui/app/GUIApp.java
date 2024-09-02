89728c42ff1126b9eb099a2f69d24c963a0f43d5
Daniel Omesi <omesidaniel@gmail.com>
2024-09-02 17:52:23 +0300

    Created new STL objects, added javafx libraries

---
+++ ui/src/gui/app/GUIApp.java
@@ -0,0 +1,23 @@
+package gui.app;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GUIApp extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+        root.getChildren().add(new Label("Hello, JavaFX!"));
+        Scene scene = new Scene(root, 300, 250);
+        primaryStage.setTitle("JavaFX Test");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
